603fda7eaa5ce096d42ddf642e68e01aace1afa4
kvoon <a1176079417@gmail.com>
2025-01-16 14:40:48 +0800

    init

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    hello, <span font-bold>world</span>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,64 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+  modules: [
+    '@nuxt/eslint',
+    'reka-ui/nuxt',
+    '@unocss/nuxt',
+    '@vueuse/nuxt',
+  ],
+
+  ssr: false,
+
+  devtools: { enabled: true },
+
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
+
+  router: {
+    options: {
+      hashMode: true,
+    },
+  },
+
+  srcDir: 'app',
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    typedPages: true,
+    clientNodeCompat: true,
+  },
+
+  compatibilityDate: '2024-11-01',
+
+  nitro: {
+    preset: 'static',
+  },
+
+  vite: {
+    base: './',
+  },
+
+  hooks: {
+    'prerender:routes': function ({ routes }) {
+      routes.clear() // Do not generate any routes (except the defaults)
+    },
+  },
+
+  eslint: {
+    config: {
+      standalone: false,
+      nuxt: {
+        sortConfigKeys: true,
+      },
+    },
+  },
+
+  unocss: {
+    nuxtLayers: true,
+  },
+})
